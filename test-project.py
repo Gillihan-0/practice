3ecd21fd737742528a3b5ff14f356f42e2efd193
Steven <steven.gillihan@gmail.com>
2018-06-21 18:08:14 -0500

    .

--- test-project.py
+++ test-project.py
@@ -3,15 +3,15 @@ def avgscript():
     count = 0
     print ("this is an average calculator.")
     variable1 = float(input("Enter how many numbers are you going to average out?\n"))
-    while count < (variable1):
-        number =  float(input("Enter a number here: \n"))
+    while count < variable1:
+        number = float(input("Enter a number here: \n"))
         total = total + number
         count = count + 1
-    average = total / (variable1)
+    average = total / variable1
     print (f"the average is {average}")
 avgscript()
 
 question = input("Do you wish to calculate again? y/n: \n")
 
-if question == ("y"):
+if question == "y":
     avgscript()
