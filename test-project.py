e501a5050f687c742d90ab7a091d3eebf07aa6ac
Gillihan-0 <40472305+Gillihan-0@users.noreply.github.com>
2018-06-21 18:04:25 -0500

    Add files via upload

---
+++ test-project.py
@@ -0,0 +1,17 @@
+def avgscript():
+    total = 0.0
+    count = 0
+    print ("this is an average calculator.")
+    variable1 = float(input("Enter how many numbers are you going to average out?\n"))
+    while count < (variable1):
+        number =  float(input("Enter a number here: \n"))
+        total = total + number
+        count = count + 1
+    average = total / (variable1)
+    print (f"the average is {average}")
+avgscript()
+
+question = input("Do you wish to calculate again? y/n: \n")
+
+if question == ("y"):
+    avgscript()
