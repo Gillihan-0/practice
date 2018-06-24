156c5fe48e70e33c122857704be7e52fdf860009
Steven <steven.gillihan@gmail.com>
2018-06-24 14:42:39 -0500

    .

--- test-project.py
+++ test-project.py
@@ -1,17 +1,38 @@
-def avgscript():
-    total = 0.0
-    count = 0
-    print ("this is an average calculator.")
-    variable1 = float(input("Enter how many numbers are you going to average out?\n"))
-    while count < variable1:
-        number = float(input("Enter a number here: \n"))
-        total = total + number
-        count = count + 1
-    average = total / variable1
-    print (f"the average is {average}")
-avgscript()
+# def avgscript():
+#     total = 0.0
+#     count = 0
+#     print ("this is an average calculator.")
+#     variable1 = float(input("Enter how many numbers are you going to average out?\n"))
+#     while count < variable1:
+#         number = float(input("Enter a number here: \n"))
+#         total = total + number
+#         count = count + 1
+#     average = total / variable1
+#     print (f"the average is {average}")
+# avgscript()
+#
+# question = input("Do you wish to calculate again? y/n: \n")
+#
+# if question == "y":
+#     avgscript()
 
-question = input("Do you wish to calculate again? y/n: \n")
 
-if question == "y":
-    avgscript()
+
+# score = input("Etner a score:")
+# score = int(score)
+# if score >= 80:
+#     grade = 'A'
+# else:
+#     if score >= 70:
+#         grade = 'B'
+#     else:
+#         if score >= 55:
+#             grade = 'C'
+#         else:
+#             if score >= 50:
+#                 grade = 'Pass'
+#             else:
+#                 grade= 'Fail'
+# print("\n\nGrade is: " + grade)
+
+
