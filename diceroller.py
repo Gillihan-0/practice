3ebdfe56b3c810b3e38e1e10f47ebca3de25a5b2
Steven <steven.gillihan@gmail.com>
2018-07-13 17:47:48 -0500

    .

--- diceroller.py
+++ diceroller.py
@@ -17,22 +17,22 @@ def rollDice(dice, sides):
 if __name__ == "__main__":
 
     print("Welcome to Dicey's Ultimate Dice Thrower.")
-    sides = 1
-    dice = 1
+    sides = 6
+    dice = 2
     print(f"Currently rolling {dice}d{sides}")
     while True:
         print()
         command = input("[R]oll the dice, [C]hange die or [Q]uit.\n")
-        if command == 'r' or command == 'R':
+        if command == 'r' or command == 'R' or command == 'roll' or command == "Roll" or command == "ROLL":
             dicetotal = rollDice(dice, sides)
             print(f"The dice total {dicetotal}")
         elif command == 'c' or command == 'C':
             try:
-                sides = int(input("How many faces are on your die?\n"))
                 dice = int(input("How many dice are you going to roll?\n"))
+                sides = int(input("How many faces are on your die?\n"))
                 if sides < 1 or dice < 1:
                     print()
-                    print("Your (non-existent) die was eaten by a grue. You have lost.")
+                    print("Your (non-existent) die was eaten by a grue. Game over.")
                     quit()
             except ValueError:
                 print()
