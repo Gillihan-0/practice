5ecccf50891e5526fe4cd34eccb76d9d45162e4c
Steven <steven.gillihan@gmail.com>
2018-07-14 18:49:11 -0500

    Added message to rollDice function indicating that the player needs to chose a positive integer.

--- diceroller.py
+++ diceroller.py
@@ -3,27 +3,32 @@ import random
 
 
 def rollDice(dice, sides):
-    print(f"Rolling {dice}d{sides}...")
     total = 0
-    for x in range(0, dice):
-        die = random.randint(1, sides)
-        total += die
-        print()
-        print(f"--= [{die}] =--")
-    print()
-    return total
+    if dice > 1 and sides > 1:
+        print(f"Rolling {dice}d{sides}...")
+        for x in range(0, dice):
+            die = random.randint(1, sides)
+            total += die
+            print()
+            print(f"--= [{die}] =--")
+            print()
+        return total
+    else:
+        print("Please chose a valid die configuration.")
 
 
 if __name__ == "__main__":
-
+    print()
+    print()
     print("Welcome to Dicey's Ultimate Dice Thrower.")
+    print()
+    print()
     sides = 6
     dice = 2
     print(f"Currently rolling {dice}d{sides}")
     while True:
-        print()
         command = input("[R]oll the dice, [C]hange die or [Q]uit.\n")
-        if command == 'r' or command == 'R' or command == 'roll' or command == "Roll" or command == "ROLL":
+        if command == 'r' or command == 'R':
             dicetotal = rollDice(dice, sides)
             print(f"The dice total {dicetotal}")
         elif command == 'c' or command == 'C':
@@ -31,18 +36,11 @@ if __name__ == "__main__":
                 dice = int(input("How many dice are you going to roll?\n"))
                 sides = int(input("How many faces are on your die?\n"))
                 if sides < 1 or dice < 1:
-                    print()
-                    print("Your (non-existent) die was eaten by a grue. Game over.")
-                    quit()
+                    print("Chose a positive, nonzero number.")
             except ValueError:
-                print()
                 print("Invalid input. Input must be an integer. Try again.")
-                print()
         elif command == 'q' or command == 'Q':
-            print()
             print("Thanks for using Dicey's Ultimate Dice Thrower.")
             break
         else:
-            print()
             print("Invalid input. Try again.")
-            print()
